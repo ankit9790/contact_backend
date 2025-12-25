31c9313cd2bde303e2c3d2c1749e74e95675baf7
ankit <ankity9790@gmail.com>
2025-12-25 17:37:36 +0530

    fic db

--- connection/db.js
+++ connection/db.js
@@ -1,16 +1,21 @@
-import dotenv from "dotenv";
 import mysql from "mysql2";
-// ✅ LOAD ENV HERE (CRITICAL FIX)
-dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
-db.getConnection((err) => {
-  if (err) console.log("DB Error:", err);
-  else console.log("MySQL Connected ✅");
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("DB Connection Failed ❌", err.message);
+  } else {
+    console.log("MySQL Connected ✅");
+    connection.release();
+  }
 });
